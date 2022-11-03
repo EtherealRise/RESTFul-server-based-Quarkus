7e35c2766121e2832c4a4fa3d5fd6d71d5621e56
Yuanzhe Liu <945695107@qq.com>
2022-11-03 20:15:33 +0000

    Set testing enviroment
    add one customer and one flight for the booking resource testing.
    set testing port explicitly at 8083 to aviod confict.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,7 @@
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-1');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-2');
 -- insert into myentity (id, field) values(nextval('hibernate_sequence'), 'field-3');
+
+insert into Customer (id, name, phoneNumber, email) values(nextval('customerId_seq'), 'customer', '01234567890', 'customer@email.com');
+
+insert into Flight (id, number, departure, destination) values(nextval('flightId_seq'), 'A1234', 'ABC', 'XYZ');
